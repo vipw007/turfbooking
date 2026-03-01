10c9ffc617318c678efd8c22aed053ba25c35116
vivekyadav <vivekkumaryadav97@gmail.com>
2026-03-01 16:07:57 +0530

    feat: firebase fixes

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFunctions } from "firebase/functions";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoHKfF6ulBQKJAvqC8Pv8U_n70msmr3OE",
-  authDomain: "turfbooking-cc0af.firebaseapp.com",
-  projectId: "turfbooking-cc0af",
-  storageBucket: "turfbooking-cc0af.firebasestorage.app",
-  messagingSenderId: "606674146499",
-  appId: "1:606674146499:web:07df34c6f685a1ac20fe76",
-  measurementId: "G-ECM7Q9QVHK"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
